ce9385ecc9ae8240d5e5fe549685f48062e421e9
jensonjenkins <dave.martheen@gmail.com>
2023-01-29 15:51:19 +0800

    "last commit"

--- src/App.js
+++ src/App.js
@@ -2,25 +2,30 @@ import Navbar from './components/Navbar';
 import Topside from './components/Topside';
 import MainD from './data/MainD';
 import LineChart from './trial/LineChart';
+
 import { UserData } from './trial/Data';
 import { useState } from 'react';
 import './App.css';
-import Axios from 'axios';
 
 function App() {
-  
+
   const [userData, setUserData] = useState({
-    labels: UserData.map((data) => data.year),
+    labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,],
     datasets: [
       {
         label: "Users Gained",
-        data: UserData.map((data) => data.userGain),
+        data: [1, 2, 6, 4, 8, 5, 8, 2, 9, 1, 0, 5, 7, 9],
         backgroundColor: [
           "rgba(75,192,192,1)",
         ],
         borderColor: 'rgb(75, 192, 192)',
-        tension: 0.2,
-        borderWidth: 1,
+        tension: 0.3,
+        pointRadius: 1,
+        borderWidth: 2,
+        fill: {
+          target: 'origin',
+          above: 'rgba(75, 192, 192, 0.2)',   // Area will be red above the origin   // And blue below the origin
+        }
       },
     ],
   });
