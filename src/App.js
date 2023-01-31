b0a71048700502a9a986257f6401c2cb6af9679a
jensonjenkins <dave.martheen@gmail.com>
2023-01-31 21:50:06 +0800

    useeffect

--- src/App.js
+++ src/App.js
@@ -1,40 +1,20 @@
 import Navbar from './components/Navbar';
 import Topside from './components/Topside';
 import MainD from './data/MainD';
-import LineChart from './trial/LineChart';
-
-import { UserData } from './trial/Data';
+import { Line } from "react-chartjs-2";
 import { useState } from 'react';
+
 import './App.css';
 
 function App() {
-
-  const [userData, setUserData] = useState({
-    labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,],
-    datasets: [
-      {
-        label: "Users Gained",
-        data: [1, 2, 6, 4, 8, 5, 8, 2, 9, 1, 0, 5, 7, 9],
-        backgroundColor: [
-          "rgba(75,192,192,1)",
-        ],
-        borderColor: 'rgb(75, 192, 192)',
-        tension: 0.3,
-        pointRadius: 1,
-        borderWidth: 2,
-        fill: {
-          target: 'origin',
-          above: 'rgba(75, 192, 192, 0.2)',   // Area will be red above the origin   // And blue below the origin
-        }
-      },
-    ],
-  });
+ 
   return (
     <>
       <Navbar />
       <Topside></Topside>
       <MainD></MainD>
-      <LineChart chartData={userData} />
+      {/* <Line data = {graph}/> */}
+
     </>
   );
 }
--- src/data/MainD.js
+++ src/data/MainD.js
@@ -1,22 +1,74 @@
 import Axios from "axios"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import './MainD.css'
+import { Line } from "react-chartjs-2";
+import { Chart as ChartJS } from "chart.js/auto";
+import Plot from 'react-plotly.js';
 
+export const getStaticProps = async()=> {
 
+}
 const MainD = () => {
-    const curAr = [];
-    const getVal = () => {
-        Axios.get('https://api.twelvedata.com/time_series?apikey=2ff55c0bf8614eb5a7bc3f68cee19b31&interval=1min&format=JSON&symbol=AAPL').then((response) => {
+    let StockSymbol = "AMZN";
+    let API_KEY = "KHM0G6B8QHEQ0A02"
 
-            for (let i = 0; i <= 29; i++) {
-                curAr.push(response.data.values[i].open);
+    const [getValueX, setGetValueX] = useState([])
+    const [getValueY, setGetValueY] = useState([])
+    const [dummy, setDummy] = useState('')
+
+    useEffect(()=>{
+        Axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${StockSymbol}&interval=5min&outputsize=full&apikey=${API_KEY}`).then((response) => {
+            let DummyVal = [];
+            for (var key in response.data['Time Series (5min)']) {
+                DummyVal.push(parseFloat(response.data['Time Series (5min)'][key]['1. open']))
+                
             }
+            setDummy(DummyVal) 
         })
-        console.log(curAr);
-    }
+    }, [dummy])
+
+    useEffect(()=>{
+        Axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${StockSymbol}&interval=5min&outputsize=full&apikey=${API_KEY}`).then((response) => {
+            let FinalYValues = [];
+            for (var key in response.data['Time Series (5min)']) {
+                FinalYValues.push(parseFloat(response.data['Time Series (5min)'][key]['1. open']))
+                
+            }
+            setGetValueY(FinalYValues) 
+        })
+
+    }, [])
+        
+    useEffect(()=>{
+
+        Axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${StockSymbol}&interval=5min&outputsize=full&apikey=${API_KEY}`).then((response) => {
+            let FinalXValues = [];
+            for (var key in response.data['Time Series (5min)']) {
+                FinalXValues.push(key);
+
+            }
+            setGetValueX(FinalXValues)
+        })
+
+    }, [])
+
+    console.log(getValueX)
+    console.log(getValueY)
+
     return (
         <>
-            <button onClick={getVal}>getVal</button>
+            <Plot
+                data={[
+                    {
+                        x: getValueX,
+                        y: getValueY,
+                        type: 'scatter',
+                        mode: 'lines+markers',
+                        marker: { color: 'rgb(75, 192, 192)' },
+
+                    }
+                ]}
+                layout={{ width: 720, height: 440 }} />
         </>
     )
 };
